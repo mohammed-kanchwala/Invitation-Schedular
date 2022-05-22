1f4781f42e3ab0e2344a8d618a922e1ccc60b573
Mohammed Kanchwala <mohammed.kanchwala52@gmail.com>
2022-05-22 17:28:47 +0400

    adding validation to not allow negative value for distance

--- src/main/java/com/washmen/controller/PartnerController.java
+++ src/main/java/com/washmen/controller/PartnerController.java
@@ -1,17 +1,26 @@
 package com.washmen.controller;
 
+import com.washmen.entity.Partner;
+import com.washmen.model.PartnerResponse;
 import com.washmen.service.PartnerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 @Slf4j
+@Validated
 public class PartnerController {
 
     private final PartnerService partnerService;
@@ -22,13 +31,14 @@ public class PartnerController {
     }
 
     @GetMapping("/partners")
-    public ResponseEntity<?> listAll() {
+    public ResponseEntity<List<Partner>> listAll() {
         log.info("PartnerController:  list partners");
         return ResponseEntity.ok(partnerService.listAll());
     }
 
     @GetMapping("/search")
-    public ResponseEntity<?> fetchPartners(@RequestParam double distance) {
+    public ResponseEntity<List<PartnerResponse>> fetchPartners(@Valid @RequestParam @Positive
+                                                                   @Min(value = 1L, message = "value of distance must be positive") Double distance) {
         log.info("PartnerController:  fetch partners");
         return ResponseEntity.ok(partnerService.fetchPartners(distance));
     }
